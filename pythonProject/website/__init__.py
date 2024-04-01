929bff052f854158ab816072fe9481ee1230bad4
Adam Małysz <adam.malysz3@onet.eu>
2024-04-01 12:59:59 +0200

    backend is ready

--- pythonProject/website/__init__.py
+++ pythonProject/website/__init__.py
@@ -11,10 +11,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "secretkey"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-
-
-
     db.init_app(app)
+
     from .views import views
 
     app.register_blueprint(views, url_prefix='/')
@@ -23,5 +21,13 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'views.index'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return Admin.query.get(int(id))
 
     return app
--- pythonProject/website/views.py
+++ pythonProject/website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, json, jsonify, redirect
-from flask_login import login_required, logout_user, current_user
+from flask_login import login_required, logout_user, current_user, login_user
 from sqlalchemy.orm import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy import select, engine
@@ -12,6 +12,7 @@ views = Blueprint('views', __name__)
 # db.session.commit()
 @views.route('/', methods=['POST', 'GET'])
 def index():
+    print(current_user)
     if request.method == 'POST' and request.form != None:
         SkladnikiUsera=""
         id=0
@@ -57,6 +58,16 @@ def index():
 
 @views.route('/admin', methods=['POST', 'GET'])
 def admin():
+    if current_user.is_authenticated:
+        if db.session.query(Przepisy).count() >= 1:
+            przepisyz = db.session.query(Przepisy)
+        else:
+            przepisyz = None
+        if db.session.query(Skladniki).count() >= 1:
+            skladniki = db.session.query(Skladniki)
+        else:
+            skladniki = None
+        return render_template('admin.html',Przepisy=przepisyz,Skladniki=skladniki)
     #print(db.session.query(Przepisy).count())
     if request.method == 'POST':
         name = request.form.get('Admin')
@@ -65,14 +76,13 @@ def admin():
             user = Admin.query.filter_by(name=name).first()
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 if db.session.query(Przepisy).count() >= 1:
                     przepisyz = db.session.query(Przepisy)
-                    for x in przepisyz:
-                        print(x.id, x.nazwa, x.czas, x.opis, x.skladniki, x.przepis, x.ListaSkladnikow)
                 else:
                     przepisyz = None
                 if db.session.query(Skladniki).count() >= 1:
-                    skladniki = select(Skladniki)
+                    skladniki = db.session.query(Skladniki)
                 else:
                     skladniki = None
                 return render_template('admin.html',Przepisy=przepisyz,Skladniki=skladniki)
@@ -87,21 +97,81 @@ def admin():
         return render_template('Login.html')
 @views.route('/admin/Przepisy', methods=['POST', 'GET'])
 def przepisy():
-    if request.method == 'POST':
-        nazwa=request.form.get('nazwa')
-        czas=request.form.get('czas')
-        opis=request.form.get('opis')
-        skladniki=request.form.get('skladniki')
-        przepis=request.form.get('przepis')
-        ListaSkladnikow=request.form.get('lista')
-        przepis = Przepisy(nazwa=nazwa,czas=czas,opis=opis,skladniki=skladniki,przepis=przepis,ListaSkladnikow=ListaSkladnikow)
-        db.session.add(przepis)
-        db.session.commit()
-        flash('Przepis został dodany!', category='success')
-        return redirect('/admin')
-    return render_template('przepisy.html')
+    if current_user.is_authenticated:
+        if request.method == 'POST':
+            nazwa=request.form.get('nazwa')
+            czas=request.form.get('czas')
+            opis=request.form.get('opis')
+            skladniki=request.form.get('skladniki')
+            przepis=request.form.get('przepis')
+            ListaSkladnikow=request.form.get('lista')
+            przepis = Przepisy(nazwa=nazwa,czas=czas,opis=opis,skladniki=skladniki,przepis=przepis,ListaSkladnikow=ListaSkladnikow)
+            db.session.add(przepis)
+            db.session.commit()
+            flash('Przepis został dodany!', category='success')
+            return redirect('/admin')
+        return render_template('przepisy.html',)
 @views.route('/admin/Skladniki', methods=['POST', 'GET'])
 def skladniki():
-    if request.method == 'POST':
-        pass
-    return redirect('/admin')
+    if current_user.is_authenticated:
+        if request.method == 'POST':
+            Nazwa=request.form.get('Nazwa')
+            kategoria=request.form.get('kategoria')
+            skladniki = Skladniki(Nazwa=Nazwa,kategoria=kategoria)
+            db.session.add(skladniki)
+            db.session.commit()
+            flash('Skladnik został dodany!', category='success')
+            return redirect('/admin')
+        return render_template('skladniki.html')
+
+@views.route('/logout')
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
+        return redirect('/')
+@views.route('/admin/delete/przepis/<int:id>')
+def delete(id):
+    if current_user.is_authenticated:
+        przepis = Przepisy.query.filter_by(id=id).first()
+        db.session.delete(przepis)
+        db.session.commit()
+        flash('Przepis został usunięty!', category='success')
+        return redirect('/admin')
+@views.route('/admin/delete/skladnik/<int:id>')
+def deleteS(id):
+    if current_user.is_authenticated:
+        skladnik = Skladniki.query.filter_by(id=id).first()
+        db.session.delete(skladnik)
+        db.session.commit()
+        flash('Skłądnik został usunięty!', category='success')
+        return redirect('/admin')
+
+@views.route('/admin/edit/skladnik/<int:id>', methods=['POST', 'GET'])
+def edit(id):
+        if current_user.is_authenticated:
+            skladnik = Skladniki.query.filter_by(id=id).first()
+            if request.method == 'POST':
+                skladnik.Nazwa = request.form.get('Nazwa')
+                skladnik.kategoria = request.form.get('kategoria')
+                db.session.commit()
+                flash('Skladnik został edytowany!', category='success')
+                return redirect('/admin')
+            return render_template('editSkladnik.html',skladnik=skladnik)
+@views.route('/admin/edit/przepis/<int:id>', methods=['POST', 'GET'])
+def editP(id):
+    if current_user.is_authenticated:
+        przepis = Przepisy.query.filter_by(id=id).first()
+        if request.method == 'POST':
+            przepis.nazwa = request.form.get('nazwa')
+            przepis.czas = request.form.get('czas')
+            przepis.opis = request.form.get('opis')
+            przepis.skladniki = request.form.get('skladniki')
+            przepis.przepis = request.form.get('przepis')
+            przepis.ListaSkladnikow = request.form.get('lista')
+            db.session.commit()
+            flash('Przepis został edytowany!', category='success')
+            return redirect('/admin')
+        return render_template('editPrzepis.html',przepis=przepis)
+
+
+
