1f634eb9f4fcf770589761e3d6a6b927db847a6f
Zentyl <matszc32@wp.pl>
2024-04-14 17:48:20 +0200

    displaying recipes information in user panel

--- pythonProject/website/static/js/script.js
+++ pythonProject/website/static/js/script.js
@@ -21,18 +21,6 @@ function podajIlosc(idProduktu, nazwaProduktu) {
     pole.append(document.createElement("br"));
     pole.append(nazwa);
     pole.append(ilosc);
-    pole.append(document.createElement("br"));
 
     formField.append(pole);
-}
-
-function wyswietlPrzepisy() {
-    let snaf = document.querySelector(".przepisySnafa");
-    let line = document.createElement('hr');
-    snaf.append(line);
-    let przepisy = document.createElement("p");
-    przepisy.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    snaf.append(przepisy);
-    const button = document.getElementById("btnGotowe");
-    button.setAttribute("disabled", "");
 }
