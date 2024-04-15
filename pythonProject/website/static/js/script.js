cec4be975428532b4944710a18cc44857c2893c1
Mateusz Szczerba <114860107+Zentyl@users.noreply.github.com>
2024-04-15 16:06:27 +0200

    Ingredients can be selected once

--- pythonProject/website/static/js/script.js
+++ pythonProject/website/static/js/script.js
@@ -1,26 +1,33 @@
+let wybrane = [];
 let formField = document.getElementById('formField');
-
 document.getElementById("btnGotowe").style.display="none";
 function podajIlosc(idProduktu, nazwaProduktu) {
-    document.getElementById("btnGotowe").style.display = "block";
+    console.log(wybrane);
+    if(wybrane.includes(idProduktu)) {
+        return false;
+    }
+    else {
+        wybrane.push(idProduktu);
+        document.getElementById("btnGotowe").style.display = "block";
 
-    let pole = document.createElement("div");
-    pole.setAttribute("id", "pole");
+        let pole = document.createElement("div");
+        pole.setAttribute("id", "pole");
 
-    let nazwa = document.createElement("label");
-    nazwa.setAttribute("id", "nazwaProduktu");
-    nazwa.setAttribute("for", idProduktu);
-    nazwa.innerHTML = nazwaProduktu;
+        let nazwa = document.createElement("label");
+        nazwa.setAttribute("id", "nazwaProduktu");
+        nazwa.setAttribute("for", idProduktu);
+        nazwa.innerHTML = nazwaProduktu;
 
-    let ilosc = document.createElement("input");
-    ilosc.setAttribute("type", "text");
-    ilosc.setAttribute("id", idProduktu);
-    ilosc.setAttribute("name",idProduktu);
-    ilosc.setAttribute("style","display:none;");
+        let ilosc = document.createElement("input");
+        ilosc.setAttribute("type", "text");
+        ilosc.setAttribute("id", idProduktu);
+        ilosc.setAttribute("name",idProduktu);
+        ilosc.setAttribute("style","display:none;");
 
-    pole.append(document.createElement("br"));
-    pole.append(nazwa);
-    pole.append(ilosc);
+        pole.append(document.createElement("br"));
+        pole.append(nazwa);
+        pole.append(ilosc);
 
-    formField.append(pole);
+        formField.append(pole);
+    }
 }
