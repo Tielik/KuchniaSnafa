bcc6d48dbdfc4b2de1a6ade2e60ab4194120b627
Adam Małysz <adam.malysz3@onet.eu>
2024-04-21 13:56:56 +0200

    added śpiace api

--- pythonProject/website/__init__.py
+++ pythonProject/website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_restful import Api, Resource, fields, marshal_with
 import sqlite3
 from flask import g
 
@@ -12,6 +13,7 @@ def create_app():
     app.config['SECRET_KEY'] = "secretkey4323423 23423"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    api=Api(app)
 
     from .views import views
     from .admin import admin
@@ -27,6 +29,33 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = 'views.index'
     login_manager.init_app(app)
+
+    resource_fields={
+         'id': fields.Integer,
+         'nazwa': fields.String,
+         'czas': fields.String,
+         'opis': fields.String,
+         'skladniki': fields.String,
+         'przepis': fields.String,
+         'ListaSkladnikow': fields.String
+     }
+    resource_fields2 = {
+        'id': fields.Integer,
+        'Nazwa': fields.String,
+        'kategoria': fields.Integer
+    }
+    class PrzepisyAPI(Resource):
+        @marshal_with(resource_fields)
+        def get(self):
+            return Przepisy.query.all()
+
+    class SkladnikiAPI(Resource):
+        @marshal_with(resource_fields2)
+        def get(self):
+            return Skladniki.query.all()
+
+    api.add_resource(PrzepisyAPI, '/API/przepisy')
+    api.add_resource(SkladnikiAPI, '/API/skladniki')
 
     @login_manager.user_loader
     def load_user(id):
