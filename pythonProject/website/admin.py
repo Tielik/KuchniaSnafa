96818ff3dd56c01c26dd97b8bae04ec5b30b1d49
Zentyl <matszc32@wp.pl>
2024-06-09 21:24:50 +0200

    Translation fix

--- pythonProject/website/admin.py
+++ pythonProject/website/admin.py
@@ -13,9 +13,9 @@ admin = Blueprint('admin', __name__)
 
 """
 Function for handling the admin index page. 
-Retrieves and displays 'Przepisy' and 'Skladniki' data if the user is authenticated. 
+Retrieves and displays 'Dish' and 'Ingredient' data if the user is authenticated. 
 If not authenticated, handles user login and authentication. 
-Returns the rendered admin page template with 'Przepisy' and 'Skladniki' data if authenticated, else, redirects to the login page.
+Returns the rendered admin page template with 'Dish' and 'Ingredient' data if authenticated, else, redirects to the login page.
 """
 @admin.route('/', methods=['POST', 'GET'])
 def index_admin():
@@ -93,11 +93,11 @@ def change_password():
     Returns:
     - If the current user is not authenticated, it redirects to the '/' route.
     - If the current user is authenticated:
-        - If there are no Skladniki in the database, it sets ingredients to None and flashes an error message.
-        - If there are Skladniki in the database, it queries all Skladniki and assigns the result to the ingredients variable.
+        - If there are no Ingredient in the database, it sets ingredients to None and flashes an error message.
+        - If there are Ingredient in the database, it queries all Ingredient and assigns the result to the ingredients variable.
         - If the request method is POST:
-        - create new row in Przepisy table 
-        -create image in images folder with name the same as id of new row in Przepisy table 
+        - create new row in Dish table 
+        -create image in images folder with name the same as id of new row in Dish table 
         - If the request method is GET, it renders the 'przepisy.html' template with the ingredients variable.
 
     """
@@ -131,9 +131,9 @@ def Dishes():
             if picture_name != '':
                 picture_ext = os.path.splitext(picture_name)[1]
                 if picture_ext in ['.png', '.jpg', '.jpeg', '.webp']:
-                    # Pobranie id ostatniego przepisu jako string <Przepisy id>
+                    # Pobranie id ostatniego przepisu jako string <Dish id>
                     dish_last_id = str(db.session.query(Dish).order_by(Dish.id.desc()).first())
-                    # Utworzenie nowego id poprzez inkrementacje wyciagnietego id ze stringa <Przepisy id>
+                    # Utworzenie nowego id poprzez inkrementacje wyciagnietego id ze stringa <Dish id>
                     if(dish_last_id == "None"):
                         dish_new_id = 1
                     else:
@@ -152,14 +152,14 @@ def Dishes():
     return redirect('/')
 
     """
-    Route for handling the '/ingredients' endpoint. This endpoint is used to add new 'Skladniki' objects to the database.
+    Route for handling the '/ingredients' endpoint. This endpoint is used to add new 'Ingredient' objects to the database.
     Parameters:
     - None
     Returns:
     - If the current user is not authenticated, it redirects to the '/' route.
     - If the current user is authenticated:
         - If the request method is POST:
-            - Retrieves the 'Nazwa' and 'kategoria' values from the request form.
+            - Retrieves the 'name' and 'category' values from the request form.
             - Adds the new object to the database.
             - Flashes a success message.
             - Redirects to the '/admin' route.
@@ -170,9 +170,9 @@ def Dishes():
 def Ingredients():
     if current_user.is_authenticated:
         if request.method == 'POST':
-            Nazwa = request.form.get('Nazwa')
-            kategoria = request.form.get('kategoria')
-            ingredients = Skladniki(Nazwa=Nazwa, kategoria=kategoria)
+            name = request.form.get('name')
+            category = request.form.get('category')
+            ingredients = Ingredient(name=name, category=category)
             db.session.add(ingredients)
             db.session.commit()
             flash('Skladnik został dodany!', category='success')
@@ -216,7 +216,7 @@ def logout():
 @admin.route('/delete/przepis/<int:id>')
 def delete(id):
     if current_user.is_authenticated:
-        przepis = Przepisy.query.filter_by(id=id).first()
+        przepis = Dish.query.filter_by(id=id).first()
         grafika_name = str(id) + '.png'
         if os.path.exists(f'website/static/img/{grafika_name}'):
             os.remove(os.path.join('website/static/img', grafika_name))
@@ -239,7 +239,7 @@ def delete(id):
 @admin.route('/delete/skladnik/<int:id>')
 def deleteS(id):
     if current_user.is_authenticated:
-        skladnik = Skladniki.query.filter_by(id=id).first()
+        skladnik = Ingredient.query.filter_by(id=id).first()
         db.session.delete(skladnik)
         db.session.commit()
         dishes_with_matching_ingredient_remover(id)
@@ -254,7 +254,7 @@ def deleteS(id):
     - id (int): The ID of the 'Skladnik' object to be edited.
 
     Returns:
-    - If the user is authenticated and the request method is 'POST', the 'Nazwa' and 'kategoria' fields of the 'Skladnik' object are updated with the values from the request form. 
+    - If the user is authenticated and the request method is 'POST', the 'name' and 'category' fields of the 'Skladnik' object are updated with the values from the request form. 
     The changes are committed to the database and a success message is flashed. The user is then redirected to the '/admin' page.
     - If the user is authenticated and the request method is 'GET', the 'editSkladnik.html' template is rendered with the 'skladnik' object passed as a parameter.
     - If the user is not authenticated, the user is redirected to the home page.
@@ -262,10 +262,10 @@ def deleteS(id):
 @admin.route('/edit/skladnik/<int:id>', methods=['POST', 'GET'])
 def edit(id):
     if current_user.is_authenticated:
-        skladnik = Skladniki.query.filter_by(id=id).first()
+        skladnik = Ingredient.query.filter_by(id=id).first()
         if request.method == 'POST':
-            skladnik.Nazwa = request.form.get('Nazwa')
-            skladnik.kategoria = request.form.get('kategoria')
+            skladnik.name = request.form.get('name')
+            skladnik.category = request.form.get('category')
             db.session.commit()
             flash('Skladnik został edytowany!', category='success')
             return redirect('/admin')
@@ -288,11 +288,11 @@ def edit(id):
 @admin.route('/edit/przepis/<int:id>', methods=['POST', 'GET'])
 def editP(id):
     if current_user.is_authenticated:
-        if db.session.query(Skladniki).count() >= 1:
-            ingredients = db.session.query(Skladniki)
+        if db.session.query(Ingredient).count() >= 1:
+            ingredients = db.session.query(Ingredient)
         else:
             ingredients = None
-        przepis = Przepisy.query.filter_by(id=id).first()
+        przepis = Dish.query.filter_by(id=id).first()
         if request.method == 'POST':
             przepis.nazwa = request.form.get('nazwa')
             przepis.czas = request.form.get('czas')
